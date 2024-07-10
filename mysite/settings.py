6897c474dd33503818cf7f853759c434afe7d52b
kigdntjd <kigdntjd@gmail.com>
2024-07-10 15:52:04 +0900

    settings.py edit

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-oj!io)mzu-!(48*3$%tps_gyii++rqx@i3o0ef=de!wbb^yd2='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'kigdntjd.pythonanywhere.com']
 
 
 # Application definition
